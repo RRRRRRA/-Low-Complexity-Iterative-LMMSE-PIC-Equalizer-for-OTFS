af6fcdcc0e98c41ba5d928d189509747b6bfe1d7
RRRRRRA <97106659+RRRRRRA@users.noreply.github.com>
2025-04-15 16:39:04 +0800

    Add files via upload

---
+++ AWGN analysis/digi_chirp_gen.m
@@ -0,0 +1,21 @@
+
+Sq_size = 64;
+N = Sq_size;
+M = Sq_size;
+a = zeros(N,M);
+chirp = eye(Sq_size);
+
+invchirp = zeros(N,M);
+for i = 1:M
+    invchirp(:,i) = chirp(:,M+1-i);
+end
+xchirp = invchirp + chirp;
+
+triangle(:,1:M) = invchirp(:,1:M);
+triangle(:,M+1:2*M) = chirp(:,1:M);
+figure;
+imagesc(xchirp);\
+hold on
+figure;
+imagesc(triangle);
+    
